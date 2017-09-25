6a7d3a4ab2c3435f65102dd4eed087268de9a7a2
Geoff Williams <geoff@geoffwilliams.me.uk>
2017-09-25 14:35:04 +1000

    fixed constant error when  is set

--- examples/powershell.pp
+++ examples/powershell.pp
@@ -3,3 +3,9 @@ exec { 'test':
   command   => '& C:\fail.ps1',
   provider  => powershell,
 }
+
+exec { 'test2':
+  command   => '& C:\fail.ps1',
+  provider  => powershell,
+  creates   => 'C:\ProgramData\chocolatey',
+}
--- lib/puppet/provider/exec/powershell.rb
+++ lib/puppet/provider/exec/powershell.rb
@@ -1,6 +1,14 @@
 require 'puppet/provider/exec'
 require 'puppet/provider/exec/shell'
 
+class Puppet::Parameter::Path
+
+  def validate_path(path)
+    puts "*** MOCK PATH VALIDATIATION LOADED FROM mock powershell module geoffwilliams-powershell, all validations will pass! ***"
+    true
+  end
+end
+
 Puppet::Type.type(:exec).provide :powershell, :parent => Puppet::Provider::Exec do
   include Puppet::Util::Execution
   desc "mock exec provider for powershell that does nothing"
